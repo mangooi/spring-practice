a1cf21151d124ad51c84b3e287db34861a3a2712
Administrator <Administrator@1X53436A8EN47U5>
2018-08-02 22:01:03 +0800

    20180802ver1

--- src/com/mangooi/soundsystem/CDPlayerTest.java
+++ src/com/mangooi/soundsystem/CDPlayerTest.java
@@ -2,7 +2,6 @@ package com.mangooi.soundsystem;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
