0ae495e0cf4b74795b7d38ec9cdaa377adb6b255
mangooi <476387228@qq.com>
2018-08-02 22:33:16 +0800

    !!test

--- src/com/mangooi/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.mangooi.test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("create new java project");
-		System.out.println("20180802test");
-	}
-}
